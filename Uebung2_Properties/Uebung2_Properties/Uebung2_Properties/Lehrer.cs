26ce9986243cc5fde0272b55d492229567ea4f04
FlorianWagner56AKIF <wag210712@spengergasse.at>
2022-10-06 17:44:00 +0200

    Uebung2 wurde fertiggestellt, alle Tests true

---
+++ Uebung2_Properties/Uebung2_Properties/Uebung2_Properties/Lehrer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uebung2_Properties
+{
+    internal class Lehrer
+    {
+        private decimal? _bruttogehalt = null;
+        public string? Zuname { get; set; }
+        public string Vorname { get; set; } = "";
+        public decimal? Bruttogehalt
+        {
+            get { return _bruttogehalt; } // ich hätte besser gefunden: get { return _bruttogehalt ?? 0; } ,da hier gleich eine 
+                                          // Prüfung stattfindet für evtl. nachfolgende Methoden
+            set { 
+                if (_bruttogehalt == null) { 
+                    _bruttogehalt = value;
+                }
+            }
+        }
+        public string Kuerzel { 
+            get { return Zuname?.Substring(0, 3).ToUpper() ?? ""; } 
+        }
+
+        public decimal? Nettogehalt { 
+            get {
+                return Bruttogehalt*0.8M ?? 0;
+            }
+        }
+    }
+}
---
+++ Uebung2_Properties/Uebung2_Properties/Uebung2_Properties/Rechteck.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uebung2_Properties
+{
+    internal class Rechteck
+    {
+        //Attribute werden in C# mit Kleinbuchstaben geschrieben --> Properties mit Großbuchstaben
+        private int _laenge = 0;
+        private int _breite = 0;
+
+        public int Laenge {
+            get { return _laenge; }
+            set { _laenge = (value >= 0) ? value : throw new ArgumentException("Ungültige Länge!"); }
+        }
+        public int Breite {
+            get { return _breite; }
+            set { _breite = (value >= 0) ? value : throw new ArgumentException("Ungültige Breite!"); } 
+        }
+
+        //Readonly, da kein setter definiert wurde.
+        public int Flaeche { 
+            get { return _laenge * _breite; }
+        }
+    }
+}
